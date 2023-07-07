cdd7869a06bebe111dcab3d67cd7952875551af5
Ricou107 <eric.leroux107@gmail.com>
2023-07-07 14:13:24 +0200

    css updates

--- src/components/HeaderEmployees.jsx
+++ src/components/HeaderEmployees.jsx
@@ -1,9 +1,7 @@
 import "../css/HeaderEmployees.css"
 import LogoHeaderHrnet from "../assets/logo-wealthhealth.webp"
 import UserConnected from "./UserConnected" 
-import LastEmployees from "./LastEmployees"
 import { Link } from "react-router-dom"
-import { useSelector } from "react-redux"
 
 /**
  * Allows the header to be displayed when "logged in" (by clicking on the HRNet button for the MVP)
@@ -11,22 +9,13 @@ import { useSelector } from "react-redux"
  * @returns 'HeaderHrnet' component
  */
 export default function HeaderHrnet(props) {
-    const employees = useSelector((state) => state.employees.allEmployees)
-    const lastEmployee = employees[employees.length - 1]?.newEmployee[0];
-    const secondToLastEmployee = employees[employees.length - 2]?.newEmployee[0];
 
     return(
         <>
             <header className="header-hrnet">
                 <Link to="/hrnet"><img className="logo-header-hrnet" src={LogoHeaderHrnet} alt="Logo de Weath Health représentant une sorte de rosace verte." /></Link>
 
-                <UserConnected onClick={props.handleClick} setModalOpen={props.setModalOpen} toggleText={"Create an employee"} />  
-                <hr />
-                <section className="header-employee-added">
-                    <h2>Last employees added</h2>
-                    <LastEmployees firstName={lastEmployee?.firstName} lastName={lastEmployee?.lastName} department={lastEmployee?.department} zipCode={lastEmployee?.zipCode}/>
-                    <LastEmployees firstName={secondToLastEmployee?.firstName} lastName={secondToLastEmployee?.lastName} department={secondToLastEmployee?.department} zipCode={secondToLastEmployee?.zipCode}/>
-                </section>
+                <UserConnected onClick={props.handleClick} setModalOpen={props.setModalOpen} toggleText={"Create an employee"} />
             </header>
         </>
     )
